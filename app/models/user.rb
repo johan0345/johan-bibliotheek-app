9ab5883c85dbbdf969916004750ece6a9343e62f
Johan Ton <johan.ton@lemone.com>
2024-02-29 11:11:33 +0100

    🐛 Make username and phone action authentication only work on sign up and edit page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:username, :email, :phone]
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:email]
   enum role: { standard: 0, admin: 1 }
 
-  validates :username, uniqueness: true
+
   validates :email, uniqueness: true
-  validates :phone, uniqueness: true
+
 
 
 
